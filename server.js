aed9887b612723ca0f0d517762694066e82e0f06
sterlingwebsite <ssteele1@byupathway.edu>
2026-05-04 17:27:23 -0600

    W01 Assignment: Initial Site Creation

--- server.js
+++ server.js
@@ -29,19 +29,24 @@ app.set("views", path.join(__dirname, "src/views"));
 /**
  * Routes
  */
-app.get('/', async (req, res) => {
-    const title = 'Home';
-    res.render('home', { title });
+app.get("/", async (req, res) => {
+  const title = "Home";
+  res.render("home", { title });
 });
 
-app.get('/organizations', async (req, res) => {
-    const title = 'Our Partner Organizations';
-    res.render('organizations', { title });
+app.get("/organizations", async (req, res) => {
+  const title = "Our Partner Organizations";
+  res.render("organizations", { title });
 });
 
-app.get('/projects', async (req, res) => {
-    const title = 'Service Projects';
-    res.render('projects', { title });
+app.get("/projects", async (req, res) => {
+  const title = "Service Projects";
+  res.render("projects", { title });
+});
+
+app.get("/categories", async (req, res) => {
+  const title = "Service Project Categories";
+  res.render("categories", { title });
 });
 
 app.listen(PORT, () => {
