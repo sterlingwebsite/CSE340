02845c18b001ebb33459799a7c95a9e6b6ee8950
sterlingwebsite <ssteele1@byupathway.edu>
2026-05-12 11:33:42 -0600

    W03 Learning Activity: Route and Query Parameters

--- src/models/projects.js
+++ src/models/projects.js
@@ -24,4 +24,24 @@ const getAllProjects = async () => {
   return result.rows;
 };
 
-export { getAllProjects };
+const getProjectsByOrganizationId = async (organizationID) => {
+  const query = `
+        SELECT
+            project_id,
+            organization_id,
+            title,
+            description,
+            location,
+            project_date
+        FROM public.service_projects
+        WHERE organization_id = $1
+        ORDER BY project_date;
+    `;
+
+  const queryParams = [organizationID];
+  const result = await db.query(query, queryParams);
+
+  return result.rows;
+};
+
+export { getAllProjects, getProjectsByOrganizationId };
