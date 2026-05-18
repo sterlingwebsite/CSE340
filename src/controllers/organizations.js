36ccd669ef876d897e8676351a1eff5151795f52
sterlingwebsite <ssteele1@byupathway.edu>
2026-05-18 13:01:07 -0600

    W04 Learning Activity: Updating Data

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -2,6 +2,7 @@ import {
   getAllOrganizations,
   getOrganizationDetails,
   createOrganization,
+  updateOrganization,
 } from "../models/organizations.js";
 import { getProjectsByOrganizationId } from "../models/projects.js";
 import { body, validationResult } from "express-validator";
@@ -78,10 +79,51 @@ const processNewOrganizationForm = async (req, res) => {
   res.redirect(`/organization/${organizationId}`);
 };
 
+const showEditOrganizationForm = async (req, res) => {
+  const organizationId = req.params.id;
+  const organizationDetails = await getOrganizationDetails(organizationId);
+
+  const title = "Edit Organization";
+  res.render("edit-organization", { title, organizationDetails });
+};
+
+const processEditOrganizationForm = async (req, res) => {
+  const organizationId = req.params.id;
+
+  // Check for validation errors
+  const results = validationResult(req);
+  if (!results.isEmpty()) {
+    // Validation failed - loop through errors
+    results.array().forEach((error) => {
+      req.flash("error", error.msg);
+    });
+
+    // Redirect back to the edit organization form
+    return res.redirect("/edit-organization/" + req.params.id);
+  }
+
+  const { name, description, contactEmail, logoFilename } = req.body;
+
+  await updateOrganization(
+    organizationId,
+    name,
+    description,
+    contactEmail,
+    logoFilename,
+  );
+
+  // Set a success flash message
+  req.flash("success", "Organization updated successfully!");
+
+  res.redirect(`/organization/${organizationId}`);
+};
+
 export {
   showOrganizationsPage,
   showOrganizationDetailsPage,
   showNewOrganizationForm,
   processNewOrganizationForm,
   organizationValidation,
+  showEditOrganizationForm,
+  processEditOrganizationForm,
 };
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,8 @@ import {
   showNewOrganizationForm,
   processNewOrganizationForm,
   organizationValidation,
+  showEditOrganizationForm,
+  processEditOrganizationForm,
 } from "./controllers/organizations.js";
 import {
   showProjectsPage,
@@ -25,12 +27,18 @@ router.get("/organizations", showOrganizationsPage);
 router.get("/projects", showProjectsPage);
 router.get("/categories", showCategoriesPage);
 
+// Route to handle the edit organization form submission
+router.post("/edit-organization/:id", organizationValidation, processEditOrganizationForm);
+
 // Route to handle new organization form submission
 router.post(
   "/new-organization",
   organizationValidation,
   processNewOrganizationForm,
 );
+
+// Route to display the edit organization form
+router.get("/edit-organization/:id", showEditOrganizationForm);
 
 // Route for new organization page
 router.get("/new-organization", showNewOrganizationForm);
